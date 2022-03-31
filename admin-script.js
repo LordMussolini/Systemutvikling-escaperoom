f4a92bf29f3776f478520294bcde3cfd00bc6411
siljehtangen <siljehtangen@outlook.com>
2022-03-31 09:35:24 +0200

    f

--- admin-script.js
+++ admin-script.js
@@ -1,12 +1,12 @@
 const tekstfelt = document.querySelector('#myTextArea');
-const button = document.querySelector('#btnKnapp');
+const button = document.querySelector('#btnSjekkKode');
 const oppgave = document.querySelector('#oppgave');
+const inpSvar = document.querySelector('#inpSvar');
+const svarDiv = document.querySelector('#svarDiv');
 
 let oppgavenummer = 1;
+let riktig = ``;
 
-btnKnapp.onlick = () =>{
-    let innhold = tekstfelt.value;
-}
 const db = firebase.firestore();
 const oppgaver = db.collection("oppgaver");
 
@@ -16,13 +16,44 @@ const hentOppgaver = async () =>{
     lagOppgaverHTML(svar.data());
 }
 const lagOppgaverHTML = (oppgaver) =>{
-    oppgave.innerHTML += `
+    riktig = oppgaver.riktig;
+    oppgave.innerHTML = `
     <div>${oppgaver.innhold}</div>
     `;
 
 }
-button.onclick = () =>{
-    oppgavenummer += 1;
-    hentOppgaver();
+// button.onclick = () =>{
+
+//     if(inpSvar.value === riktig){
+//         oppgavenummer += 1;
+//         hentOppgaver();
+//     }
+// }
+const sjekkKode = () => {
+    if(inpSvar.value.toLowerCase() == riktig){
+        // if(sideId >= 5){
+        //     svarDiv.innerHTML = `${svar[sideId].svarTekst}`;
+        //     sideId = 0;
+        //     index = 0 + ".html";
+        //     videreDiv.innerHTML = `<a class="linkVidere" href="vunnet.html">Du fullførte escaperoomet! Gå videre til premiesiden ved å trykke her.<a>`;
+        //     videreDiv.style.backgroundColor = "rgb(111, 179, 111)";
+        // }else{
+        //     svarDiv.innerHTML = `${svar[sideId].svarTekst}`;
+        //     sideId = sideId + 1;
+        //     index = sideId + ".html";
+        //     videreDiv.innerHTML = `<a class="linkVidere" href='${index}?id=${sideId}'>Du greide det! Gå videre til neste rom ved å trykke her.<a>`;
+        //     videreDiv.style.backgroundColor = "rgb(111, 179, 111)";
+        // }
+        oppgavenummer += 1;
+        hentOppgaver();
+    } else {
+        // svarDiv.innerHTML = `${svar[sideId].svarTekstFeil}`;
+        // videreDiv.innerHTML = `<a class="linkVidere" href='${index}?id=${sideId}'>Du greide det dessverre ikke, men trykk her for å prøve på nytt.<a>`;
+        // videreDiv.style.backgroundColor = "rgb(197, 73, 73)";
+        svarDiv.innerHTML = `Feil, prøv på nytt`;
+    }
 }
+
+button.onclick = sjekkKode;
+
 hentOppgaver();
